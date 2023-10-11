a76dedad3ac224db470a253b7630ba57f09121d8
MarroneFF <pfariaj@gmail.com>
2023-10-11 12:42:14 +0100

    Added function to save work item payloads

--- tasks.py
+++ tasks.py
@@ -12,6 +12,13 @@ json = JSON()
 table = Tables()
 
 TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
+
+# JSON data keys
+COUNTRY_KEY = "SpatialDim"
+YEAR_KEY = "TimeDim"
+RATE_KEY = "NumericValue"
+GENDER_KEY = "Dim1"
+
 @task
 def produce_traffic_data():
     """
@@ -27,6 +34,8 @@ def produce_traffic_data():
     table.write_table_to_csv(traffic_data, "output/test.csv")
     filtered_data = filter_and_sort_traffic_data(traffic_data)
     filtered_data = get_latest_data_by_country(filtered_data)
+    payloads = create_work_item_payloads(filtered_data)
+    save_work_item_payloads(payloads)
 
 @task
 def consume_traffic_data():
@@ -47,16 +56,32 @@ def filter_and_sort_traffic_data(data):
     gender_key = "Dim1"
     both_genders = "BTSX"
     year_key = "TimeDim"
-    table.filter_table_by_column(data, rate_key, "<", max_rate)
-    table.filter_table_by_column(data, gender_key, "==", both_genders)
-    table.sort_table_by_column(data, year_key, False)
+    table.filter_table_by_column(data, RATE_KEY, "<", max_rate)
+    table.filter_table_by_column(data, GENDER_KEY, "==", both_genders)
+    table.sort_table_by_column(data, YEAR_KEY, False)
     return data
 
 def get_latest_data_by_country(data):
     country_key = "SpatialDim"
-    data = table.group_table_by_column(data, country_key)
+    data = table.group_table_by_column(data, COUNTRY_KEY)
     latest_data_by_country = []
     for group in data:
         first_row = table.pop_table_row(group)
         latest_data_by_country.append(first_row)
     return latest_data_by_country
+
+def create_work_item_payloads(traffic_data):
+    payloads = []
+    for row in traffic_data:
+        payload = dict(
+            country = row[COUNTRY_KEY],
+            year = row[YEAR_KEY],
+            rate = row[RATE_KEY]
+            )
+        payloads.append(payload)
+    return payloads
+
+def save_work_item_payloads(payloads):
+    for payload in payloads:
+        variables = dict(traffic_data=payload)
+        workitems.outputs.create(variables)
